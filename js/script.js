7272131eefa72fdcf723da9f7c4a6f06f5204fbd
Alexandre Ferreira <alexmlf93@gmail.com>
2020-04-06 23:53:16 -0300

    Plugin simple-slide

---
+++ js/script.js
@@ -0,0 +1,15 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    nav: false,
+    auto: true,
+    time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portifolio",
+    nav: true,
+    auto: true,
+    time: 5000,
+  });
+}
