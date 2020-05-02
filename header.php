b5e168f5a53ea9341d255d75745572def12c580b
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-02 11:59:57 -0300

    Título do site em formato wordpress

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Bikcraft - Bicicletas Personalizadas</title>
+  <title><?php bloginfo('name'); ?></title>
 
   <meta name="description" content="Compre a sua bicileta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
   <meta property="og:type" content="website">
-  <meta property="og:title" content="Bikcraft - Bicicletas Personalizadas">
+  <meta property="og:title" content="<?php bloginfo('name'); ?>">
   <meta property="og:description" content="Compre a sua bicileta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
   <meta property="og:url" content="http://bikcraft.com">
   <meta property="og:image" content="http://bikcraft.com/img/og-image.png">
