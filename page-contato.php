4b66a8e75259505531f85f9d144fc5b94ff1ca9a
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-04 13:09:03 -0300

    Extraído introdução das páginas para criar um include

--- page-contato.php
+++ page-contato.php
@@ -4,12 +4,7 @@ get_header();
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<section class="introducao-interna interna_contato">
-			<div class="container">
-				<h1>Contato</h1>
-				<p>tire suas dúvidas com a gente</p>
-			</div>
-		</section>
+		<?php include(TEMPLATEPATH . "/inc/introducao.php")?>
 
 		<section class="contato container animar-interno">
 			<form action="enviar.php" method="post" name="form" class="formphp contato_form grid-8">
